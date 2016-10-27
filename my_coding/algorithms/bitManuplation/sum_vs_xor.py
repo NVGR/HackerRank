0ad0b3b1c8c160ff88e67d35a4e2565455b336ca
venugopal <venugopala_r_nakka@homedepot.com>
2016-10-26 23:58:31 -0500

    remove unwanted stuff

--- my_coding/algorithms/bitManuplation/sum_vs_xor.py
+++
@@ -1,4 +0,0 @@
-
-if __name__ == '__main__':
-    n = raw_input().strip()
-    print 2**(bin(n)[2:].count('0')) if n else 1
--- my_coding/algorithms/implementation/sherlock_squares.py
+++
@@ -1,6 +0,0 @@
-import math
-
-if __name__ == '__main__':
-    for _ in xrange(int(raw_input().strip())):
-        n, m = [int(x) for x in raw_input().split()]
-        print int(math.floor(m ** 0.5) - math.ceil(n ** 0.5)) + 1
